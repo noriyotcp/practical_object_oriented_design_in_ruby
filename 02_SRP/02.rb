cfd04a6bd8aabaee61f0376e05ad931165b6957b
noriyotcp <noriyo.akita@gmail.com>
2017-07-31 18:09:06 +0900

    Add rim, tire and gear_inches

--- 02_SRP/02.rb
+++ 02_SRP/02.rb
@@ -9,17 +9,26 @@ ratio = chainring / cog.to_f
 puts ratio # => 1.1111111111111112
 
 class Gear
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
 
   def ratio
     chainring / cog.to_f
   end
+
+  def gear_inches
+    ratio * (rim + (tire * 2))
+  end
 end
 
-puts Gear.new(52, 11).ratio
-puts Gear.new(30, 27).ratio
+puts Gear.new(52, 11, 26, 1.5).gear_inches # => 137.0909090909091
+puts Gear.new(52, 11, 24, 1.25).gear_inches # => 125.27272727272728
+
+# puts Gear.new(52, 11).ratio
+# /practical_object_oriented_design_in_ruby/02_SRP/02.rb:14:in `initialize': wrong number of arguments (given 2, expected 4) (ArgumentError)
