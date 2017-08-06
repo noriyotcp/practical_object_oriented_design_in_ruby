2d890070e7936b2ff77e40cc50710d1d9e457c7f
noriyotcp <noriyo.akita@gmail.com>
2017-08-06 16:22:15 +0900

    Refactor via dependency injection

--- 03_Managing_Dependencies/01.rb
+++ 03_Managing_Dependencies/01.rb
@@ -1,11 +1,10 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = wheel
   end
 
   def ratio
@@ -13,7 +12,7 @@ class Gear
   end
 
   def gear_inches
-    ratio * Wheel.new(rim, tire).diameter
+    ratio * wheel.diameter
   end
 
 end
@@ -36,4 +35,4 @@ class Wheel
   end
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
